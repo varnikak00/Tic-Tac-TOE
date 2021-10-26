22b42ecfb1e274ed8ff28b5b46a0364a5e1a93d3
varnikak00 <varnikak7070@gmail.com>
2021-10-26 19:38:30 +0530

    tic tac toc program

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TIC_TAC_TOE
+{
+    class Program
+    {
+        
+        char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        int player = 1;
+         int flog = 0;
+
+
+       public static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("player 1:X and player2:0");
+                Console.WriteLine("\n");
+                if (player % 2 == 0)
+                  {
+
+                        Console.WriteLine("Player 2 Chance");
+
+                    }
+
+                    else
+
+                    {
+
+                        Console.WriteLine("Player 1 Chance");
+
+                    }
+
+                Console.WriteLine("\n");
+
+
+                
